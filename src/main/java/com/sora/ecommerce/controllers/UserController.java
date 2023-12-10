42f569be0aafb5bfcd5616869e6199bc2fb29bd9
sorasora46 <sorrawit02546@gmail.com>
2023-12-11 03:00:47 +0700

    Create endpoint for creating user

--- src/main/java/com/sora/ecommerce/controllers/UserController.java
+++ src/main/java/com/sora/ecommerce/controllers/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -12,6 +14,11 @@ public class UserController {
     @GetMapping("/{id}")
     public String getUserById(@PathVariable String id) {
         return id;
+    }
+
+    @PostMapping("/create")
+    public String createUser(@RequestBody String info) {
+        return info;
     }
 
 }
