e0fb718867e11f702f04d1ed8e41b8cdd194aaba
sorasora46 <sorrawit02546@gmail.com>
2023-12-11 13:27:40 +0700

    Change response to use responseBuilder

--- src/main/java/com/sora/ecommerce/controllers/UserController.java
+++ src/main/java/com/sora/ecommerce/controllers/UserController.java
@@ -3,13 +3,17 @@ package com.sora.ecommerce.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sora.ecommerce.constants.ResponseStatus;
 import com.sora.ecommerce.models.domains.User;
 import com.sora.ecommerce.models.requests.CreateUserPayload;
+import com.sora.ecommerce.response.ResponseHandler;
 import com.sora.ecommerce.services.UserService;
 
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,15 +27,15 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable UUID id) {
+    public ResponseEntity<Object> getUserById(@PathVariable UUID id) {
         User user = userService.getUserById(id);
-        return user;
+        return ResponseHandler.responseBuilder(HttpStatus.OK, ResponseStatus.SUCCESS, user);
     }
 
     @PostMapping("/create")
-    public UUID createUser(@RequestBody CreateUserPayload payload) {
+    public ResponseEntity<Object> createUser(@RequestBody CreateUserPayload payload) {
         UUID id = userService.createUser(payload);
-        return id;
+        return ResponseHandler.responseBuilder(HttpStatus.CREATED, ResponseStatus.SUCCESS, id);
     }
 
 }
