1d3e4052323409091c957f7c4c498faa5438ce23
sorasora46 <sorrawit02546@gmail.com>
2023-12-11 12:03:03 +0700

    Create schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    UNIQUE (first_name, last_name)
+);
+
+CREATE TABLE IF NOT EXISTS credentials (
+    user_id uuid REFERENCES users(id),
+    username VARCHAR(10) NOT NULL UNIQUE,
+    hashed_password VARCHAR(50) NOT NULL
+);
