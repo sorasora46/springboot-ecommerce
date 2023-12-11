122ad232acccbeda73166397c1a8361e8e37c70d
sorasora46 <sorrawit02546@gmail.com>
2023-12-11 12:04:09 +0700

    Return data from userService for create and get by id

--- src/main/java/com/sora/ecommerce/controllers/UserController.java
+++ src/main/java/com/sora/ecommerce/controllers/UserController.java
@@ -2,6 +2,14 @@ package com.sora.ecommerce.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sora.ecommerce.models.domains.User;
+import com.sora.ecommerce.models.requests.CreateUserPayload;
+import com.sora.ecommerce.services.UserService;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,14 +19,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/v1/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
-    public String getUserById(@PathVariable String id) {
-        return id;
+    public User getUserById(@PathVariable UUID id) {
+        User user = userService.getUserById(id);
+        return user;
     }
 
     @PostMapping("/create")
-    public String createUser(@RequestBody String info) {
-        return info;
+    public UUID createUser(@RequestBody CreateUserPayload payload) {
+        UUID id = userService.createUser(payload);
+        return id;
     }
 
 }
