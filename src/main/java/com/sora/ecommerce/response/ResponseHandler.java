78caa0bfa38a8a9ac7e9e9b18e00f1194dbbe22c
sorasora46 <sorrawit02546@gmail.com>
2023-12-11 13:27:00 +0700

    Create ResponseHandler.java

---
+++ src/main/java/com/sora/ecommerce/response/ResponseHandler.java
@@ -0,0 +1,18 @@
+package com.sora.ecommerce.response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseHandler {
+    public static ResponseEntity<Object> responseBuilder(HttpStatus statusCode, Boolean success, Object data) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("code", statusCode.value());
+        response.put("success", success);
+        response.put("data", data);
+
+        return new ResponseEntity<>(response, statusCode);
+    }
+}
