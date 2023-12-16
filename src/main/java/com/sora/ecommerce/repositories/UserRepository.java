2d6b35a08ad395a0f8ca8bacf593548f9fb36e54
sorasora46 <sorrawit02546@gmail.com>
2023-12-16 23:02:59 +0700

    Add custom query to check existing name and email

--- src/main/java/com/sora/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/sora/ecommerce/repositories/UserRepository.java
@@ -1,10 +1,23 @@
 package com.sora.ecommerce.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sora.ecommerce.models.domains.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    public Optional<User> findByEmail(String email);
+
+    @Query(value = "SELECT * FROM users WHERE first_name = ?1 AND last_name = ?2", nativeQuery = true)
+    public Optional<User> findByName(String firstName, String lastName);
+
+    public Boolean existsByEmail(String email);
+
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM users WHERE first_name = ?1 AND last_name = ?2)", nativeQuery = true)
+    public Boolean existsByName(String firstName, String lastName);
+
 }
