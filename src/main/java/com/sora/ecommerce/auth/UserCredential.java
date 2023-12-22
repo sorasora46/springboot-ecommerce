90f919ac0878683a4afbedb0dab5b575f23d87b8
sorasora46 <sorrawit02546@gmail.com>
2023-12-22 22:35:14 +0700

    Create UserCredential.java

---
+++ src/main/java/com/sora/ecommerce/auth/UserCredential.java
@@ -0,0 +1,47 @@
+package com.sora.ecommerce.auth;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "credentials")
+public class UserCredential {
+    private UUID userId;
+    private String username;
+    private String hashedPassword;
+
+    public UserCredential() {
+    }
+
+    public UserCredential(UUID userId, String username, String hashedPassword) {
+        this.userId = userId;
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+}
