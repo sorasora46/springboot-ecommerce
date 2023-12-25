9f5ee8fbc3c5ad107cb7e7dbff4b3d708f363353
sorasora46 <sorrawit02546@gmail.com>
2023-12-25 23:35:21 +0700

    Hash plain password

--- src/main/java/com/sora/ecommerce/services/impl/UserServiceImpl.java
+++ src/main/java/com/sora/ecommerce/services/impl/UserServiceImpl.java
@@ -5,11 +5,14 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.sora.ecommerce.auth.UserCredential;
 import com.sora.ecommerce.exceptions.ApiException;
 import com.sora.ecommerce.models.domains.User;
 import com.sora.ecommerce.models.requests.CreateUserPayload;
+import com.sora.ecommerce.repositories.UserCredentialRepository;
 import com.sora.ecommerce.repositories.UserRepository;
 import com.sora.ecommerce.services.UserService;
 
@@ -18,6 +21,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserCredentialRepository userCredentialRepository;
 
     public UserServiceImpl() {
     }
@@ -37,15 +43,25 @@ public class UserServiceImpl implements UserService {
         String firstName = payload.getFirstName();
         String lastName = payload.getLastName();
         String email = payload.getEmail();
+        String username = payload.getUsername();
+        String password = payload.getPassword();
 
         User newUser = new User(firstName, lastName, email);
+
+        var passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(password);
 
         if (userRepository.existsByEmail(email))
             throw new ApiException(HttpStatus.CONFLICT, email + " already exist");
         if (userRepository.existsByName(firstName, lastName))
             throw new ApiException(HttpStatus.CONFLICT, firstName + " " + lastName + " already exist");
+        if (userCredentialRepository.existsByUsername(username))
+            throw new ApiException(HttpStatus.CONFLICT, username + " already exist");
 
         User result = userRepository.save(newUser);
+
+        UserCredential newUserCredential = new UserCredential(result.getId(), username, hashedPassword);
+        userCredentialRepository.save(newUserCredential);
 
         return result.getId();
     }
