9fece81e58e7c478b35035dc864d176aa47557ec
sorasora46 <sorrawit02546@gmail.com>
2023-12-30 20:38:19 +0700

    Create products table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,3 +13,12 @@ CREATE TABLE IF NOT EXISTS credentials (
     username VARCHAR(10) NOT NULL UNIQUE,
     hashed_password VARCHAR(60) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    price NUMERIC(2) NOT NULL,
+    amount INTEGER NOT NULL,
+    image_paths VARCHAR(255) NOT NULL
+);
