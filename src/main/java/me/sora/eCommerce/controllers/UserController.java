2a8569c90614c7e3aebe4f24cf465bd6ee0c85f4
sorasora46 <sorrawit02546@gmail.com>
2024-07-20 21:21:43 +0700

    Added path variable

--- src/main/java/me/sora/eCommerce/controllers/UserController.java
+++ src/main/java/me/sora/eCommerce/controllers/UserController.java
@@ -1,6 +1,7 @@
 package me.sora.eCommerce.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-    @GetMapping("/test")
-    public Object greeting() {
-        return "Hello World!";
+    @GetMapping("/{id}")
+    public Object getUserById(@PathVariable String id) {
+        return id;
     }
 
 }
