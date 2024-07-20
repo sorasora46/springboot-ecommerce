8072efbab5652d6cdc01752bdac23c2427c89f0a
sorasora46 <sorrawit02546@gmail.com>
2024-07-20 21:58:51 +0700

    Removed JPA

--- src/main/java/me/sora/eCommerce/entities/User.java
+++ src/main/java/me/sora/eCommerce/entities/User.java
@@ -1,6 +1,5 @@
 package me.sora.eCommerce.entities;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -11,37 +10,24 @@ import java.util.Date;
 @Getter
 @Setter
 @RequiredArgsConstructor
-@Entity(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(unique = true, nullable = false)
     private String id;
 
-    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
-    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(name = "phone_no", nullable = false, length = 15)
     private String phoneNo;
 
-    @Column(name = "birth_date", nullable = false)
     private Date birthDate;
 
-    @Column(name = "role", nullable = false, length = 10)
     private String role;
 
-    @Column(name = "created_date", nullable = false, updatable = false)
     private Instant createdDate;
 
-    @Column(name = "updated_date")
     private Instant updatedDate;
 }
