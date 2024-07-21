20818ace772d1294570d69ee8d97e82f671a16d7
sorasora46 <sorrawit02546@gmail.com>
2024-07-21 21:34:27 +0700

    Implement get user by username, id endpoint

--- src/main/java/me/sora/eCommerce/controllers/UserController.java
+++ src/main/java/me/sora/eCommerce/controllers/UserController.java
@@ -1,5 +1,9 @@
 package me.sora.eCommerce.controllers;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.RequiredArgsConstructor;
+import me.sora.eCommerce.services.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
     @GetMapping("/{id}")
-    public Object getUserById(@PathVariable String id) {
-        return id;
+    public ResponseEntity<Object> getUserById(@NotEmpty @PathVariable String id) {
+        return ResponseEntity.ok().body(userService.getUserById(id));
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<Object> getUserByUsername(@NotEmpty @PathVariable String username) {
+        return ResponseEntity.ok().body(userService.getUserByUsername(username));
     }
 
 }
