6a17d465f4edd97699a61e51ba1e365a80a8c5ce
sorasora46 <sorrawit02546@gmail.com>
2024-07-23 22:13:00 +0700

    Create ApiResponse.java

---
+++ src/main/java/me/sora/eCommerce/dto/ApiResponse.java
@@ -0,0 +1,16 @@
+package me.sora.eCommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private String message;
+    private int status;
+    private T data;
+}
