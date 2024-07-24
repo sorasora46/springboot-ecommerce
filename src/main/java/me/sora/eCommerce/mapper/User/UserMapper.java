944bfd55d92c048cdfe9228908f4730e3766ee15
sorasora46 <sorrawit02546@gmail.com>
2024-07-24 23:00:04 +0700

    Created an instance of mapper

--- src/main/java/me/sora/eCommerce/mapper/User/UserMapper.java
+++ src/main/java/me/sora/eCommerce/mapper/User/UserMapper.java
@@ -1,11 +1,14 @@
 package me.sora.eCommerce.mapper.User;
 
+import me.sora.eCommerce.dto.User.CreateUserRequest;
 import me.sora.eCommerce.dto.User.GetUserResponse;
 import me.sora.eCommerce.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     GetUserResponse fromUserEntityToGetUserResponse(User user);
 
