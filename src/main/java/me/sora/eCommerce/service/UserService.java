cd1e5c3ca4b66e4abd9b1b4d8adbe64d29e67c4e
sorasora46 <sorrawit02546@gmail.com>
2024-07-25 00:34:37 +0700

    Init saving credential flow

--- src/main/java/me/sora/eCommerce/service/UserService.java
+++ src/main/java/me/sora/eCommerce/service/UserService.java
@@ -3,7 +3,9 @@ package me.sora.eCommerce.service;
 import lombok.RequiredArgsConstructor;
 import me.sora.eCommerce.dto.User.CreateUserRequest;
 import me.sora.eCommerce.dto.User.GetUserResponse;
+import me.sora.eCommerce.entity.Credential;
 import me.sora.eCommerce.mapper.User.UserMapper;
+import me.sora.eCommerce.repository.CredentialRepository;
 import me.sora.eCommerce.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final CredentialRepository credentialRepository;
 
     public GetUserResponse getUserById(String id) {
         var user = userRepository.findById(id).orElse(null);
@@ -26,6 +29,15 @@ public class UserService {
     public GetUserResponse createUser(CreateUserRequest request) {
         var user = UserMapper.INSTANCE.fromCreateUserRequestToUser(request);
         var savedUser = userRepository.save(user);
+
+        var credential = new Credential();
+        credential.setPassword(request.getPassword());
+        credential.setSalt("salt");
+        credential.setUser(savedUser);
+        credential.setUpdatedDate(savedUser.getCreatedDate());
+
+        credentialRepository.save(credential);
+
         return UserMapper.INSTANCE.fromUserEntityToGetUserResponse(savedUser);
     }
 
