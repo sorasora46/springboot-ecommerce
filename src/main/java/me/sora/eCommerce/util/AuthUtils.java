d97dbb857845b797a6e913cf156207faa056ae7b
sorasora46 <sorrawit02546@gmail.com>
2024-07-26 02:52:12 +0700

    Create AuthUtils.java

---
+++ src/main/java/me/sora/eCommerce/util/AuthUtils.java
@@ -0,0 +1,88 @@
+package me.sora.eCommerce.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import me.sora.eCommerce.constant.AuthConstant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.SecureRandom;
+import java.util.Date;
+
+import static me.sora.eCommerce.constant.AuthConstant.CHARACTERS;
+
+@Component
+@RequiredArgsConstructor
+public class AuthUtils {
+
+    @Value("${jwt.secret}")
+    private String JWT_SECRET;
+
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + AuthConstant.ONE_DAY * 2))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, ClaimsResolver<T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.resolve(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .decryptWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    @FunctionalInterface
+    public interface ClaimsResolver<T> {
+        T resolve(Claims claims);
+    }
+
+    public String generateSalt(int length) {
+        StringBuilder salt = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = RANDOM.nextInt(CHARACTERS.length());
+            salt.append(CHARACTERS.charAt(index));
+        }
+
+        return salt.toString();
+    }
+
+    public SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(JWT_SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
