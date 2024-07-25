788c3a18612eaf6ef548f50797b940128d2114e0
sorasora46 <sorrawit02546@gmail.com>
2024-07-26 02:56:53 +0700

    Handled login request

--- src/main/java/me/sora/eCommerce/controller/AuthController.java
+++ src/main/java/me/sora/eCommerce/controller/AuthController.java
@@ -1,7 +1,13 @@
 package me.sora.eCommerce.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.sora.eCommerce.dto.Authentication.AuthenticationRequest;
+import me.sora.eCommerce.dto.Authentication.AuthenticationResponse;
+import me.sora.eCommerce.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthenticationService authenticationService;
+
     @PostMapping("/login")
-    public Object login() {
-        return null;
+    public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody AuthenticationRequest request) {
+        var response = authenticationService.authenticate(request);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/logout")
