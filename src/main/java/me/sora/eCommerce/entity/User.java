c028ae113f2e4dc094315b5cefae93350710abb8
sorasora46 <sorrawit02546@gmail.com>
2024-07-26 02:55:22 +0700

    Remapped data columns and implement UserDetails

--- src/main/java/me/sora/eCommerce/entity/User.java
+++ src/main/java/me/sora/eCommerce/entity/User.java
@@ -5,19 +5,26 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import me.sora.eCommerce.constant.AuthConstant;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
 @Entity(name = "users")
-public class User {
+public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(unique = true, nullable = false)
+    @Column(name = "id", unique = true, nullable = false)
     private String id;
 
     @Column(name = "username", unique = true, nullable = false)
@@ -29,23 +36,27 @@ public class User {
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    @Column(name = "phone_no", nullable = false, length = 15)
+    @Column(name = "phone_no", unique = true, nullable = false, length = 15)
     private String phoneNo;
 
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false, length = 10)
-    private String role;
+    private AuthConstant.Role role;
 
     @Column(name = "created_date", nullable = false, updatable = false)
     private Instant createdDate;
 
     @Column(name = "updated_date")
     private Instant updatedDate;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Credential credential;
 
     @PrePersist
     protected void onCreate() {
@@ -57,6 +68,36 @@ public class User {
     @PreUpdate
     protected void onUpdate() {
         updatedDate = Instant.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return credential != null ? credential.getPassword() : null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
