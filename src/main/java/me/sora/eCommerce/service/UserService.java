6daa04db5c09ffe321cb2946e5e710d0a9687ccc
sorasora46 <sorrawit02546@gmail.com>
2024-07-26 02:57:34 +0700

    Added code for adding salt and hashing password

--- src/main/java/me/sora/eCommerce/service/UserService.java
+++ src/main/java/me/sora/eCommerce/service/UserService.java
@@ -1,12 +1,15 @@
 package me.sora.eCommerce.service;
 
 import lombok.RequiredArgsConstructor;
+import me.sora.eCommerce.constant.AuthConstant;
 import me.sora.eCommerce.dto.User.CreateUserRequest;
 import me.sora.eCommerce.dto.User.GetUserResponse;
 import me.sora.eCommerce.entity.Credential;
 import me.sora.eCommerce.mapper.User.UserMapper;
 import me.sora.eCommerce.repository.CredentialRepository;
 import me.sora.eCommerce.repository.UserRepository;
+import me.sora.eCommerce.util.AuthUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +18,8 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final CredentialRepository credentialRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthUtils authUtils;
 
     public GetUserResponse getUserById(String id) {
         var user = userRepository.findById(id).orElse(null);
@@ -30,9 +35,13 @@ public class UserService {
         var user = UserMapper.INSTANCE.fromCreateUserRequestToUser(request);
         var savedUser = userRepository.save(user);
 
+        String salt = authUtils.generateSalt(AuthConstant.SALT_LENGTH);
+        String password = request.getPassword();
+        String hashedPassword = passwordEncoder.encode(password + salt);
+
         var credential = new Credential();
-        credential.setPassword(request.getPassword());
-        credential.setSalt("salt");
+        credential.setPassword(hashedPassword);
+        credential.setSalt(salt);
         credential.setUser(savedUser);
         credential.setUpdatedDate(savedUser.getCreatedDate());
 
