9a284544ba92811e2ad5b042db4d3f97c6b20c51
sorasora46 <sorrawit02546@gmail.com>
2024-07-27 17:14:37 +0700

    Create ApiConstant.java

---
+++ src/main/java/me/sora/eCommerce/constant/ApiConstant.java
@@ -0,0 +1,10 @@
+package me.sora.eCommerce.constant;
+
+public class ApiConstant {
+
+    public static class ApiStatus {
+        public static boolean SUCCESS = true;
+        public static boolean FAILED = false;
+    }
+
+}
