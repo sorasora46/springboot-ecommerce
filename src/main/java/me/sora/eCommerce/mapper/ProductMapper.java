8718bee1da5c57a3347b767fa0a85bc6b8dd7427
sorasora46 <sorrawit02546@gmail.com>
2024-07-27 18:11:42 +0700

    Added mapper for entity to get product response

--- src/main/java/me/sora/eCommerce/mapper/ProductMapper.java
+++ src/main/java/me/sora/eCommerce/mapper/ProductMapper.java
@@ -1,9 +1,14 @@
 package me.sora.eCommerce.mapper;
 
+import me.sora.eCommerce.dto.Product.GetProductResponse;
+import me.sora.eCommerce.entity.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface ProductMapper {
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
+    GetProductResponse fromProductEntityToGetProductResponse(Product product);
+
 }
