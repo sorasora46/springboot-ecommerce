e95f5e000e679531cdfcbcafb7849c50a318edbe
sorasora46 <sorrawit02546@gmail.com>
2024-07-27 17:52:44 +0700

    Create Product.java

---
+++ src/main/java/me/sora/eCommerce/entity/Product.java
@@ -0,0 +1,49 @@
+package me.sora.eCommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "name", unique = true, nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private Instant createdDate;
+
+    @Column(name = "updated_date", nullable = false)
+    private Instant updatedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        var now = Instant.now();
+        createdDate = now;
+        updatedDate = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedDate = Instant.now();
+    }
+
+}
