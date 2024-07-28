cb2a954ae77f88845555522a2cf86bbffdce1092
sorasora46 <sorrawit02546@gmail.com>
2024-07-28 15:28:20 +0700

    Added unit test for data not found

--- src/test/java/me/sora/eCommerce/controllers/UserControllerTest.java
+++ src/test/java/me/sora/eCommerce/controllers/UserControllerTest.java
@@ -3,7 +3,9 @@ package me.sora.eCommerce.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.sora.eCommerce.config.AuthenticationFilter;
 import me.sora.eCommerce.constant.AuthConstant;
+import me.sora.eCommerce.constant.ErrorConstant;
 import me.sora.eCommerce.controller.UserController;
+import me.sora.eCommerce.controller.advice.CustomException;
 import me.sora.eCommerce.entity.Credential;
 import me.sora.eCommerce.entity.Product;
 import me.sora.eCommerce.entity.User;
@@ -19,6 +21,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -26,6 +29,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.time.Instant;
 import java.time.LocalDate;
 
+import static me.sora.eCommerce.constant.ApiConstant.ApiStatus.FAILED;
 import static me.sora.eCommerce.constant.ApiConstant.ApiStatus.SUCCESS;
 import static me.sora.eCommerce.constant.AuthConstant.Role.USER;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -120,6 +124,50 @@ public class UserControllerTest {
                 .andExpect(MockMvcResultMatchers
                         .jsonPath("$.result.username")
                         .value(expectedResponse.getUsername()));
+    }
+
+    @Test
+    void givenRequest_whenGetUserById_thenReturnDataNotFoundException() throws Exception {
+        // Given
+
+        // When
+        when(userService.getUserById(anyString()))
+                .thenThrow(new CustomException(ErrorConstant.DATA_NOT_FOUND, HttpStatus.NOT_FOUND));
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/users/{id}", "not found id")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers
+                        .jsonPath("$.success")
+                        .value(FAILED))
+                .andExpect(MockMvcResultMatchers
+                        .jsonPath("$.result")
+                        .value(ErrorConstant.DATA_NOT_FOUND));
+    }
+
+    @Test
+    void givenRequest_whenGetUserByUsername_thenReturnDataNotFoundException() throws Exception {
+        // Given
+
+        // When
+        when(userService.getUserByUsername(anyString()))
+                .thenThrow(new CustomException(ErrorConstant.DATA_NOT_FOUND, HttpStatus.NOT_FOUND));
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/users/username/{username}", "not found username")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers
+                        .jsonPath("$.success")
+                        .value(FAILED))
+                .andExpect(MockMvcResultMatchers
+                        .jsonPath("$.result")
+                        .value(ErrorConstant.DATA_NOT_FOUND));
     }
 
 }
