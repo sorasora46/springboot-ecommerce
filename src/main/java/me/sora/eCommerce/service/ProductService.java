c6f192ebffc1ae05dbd56c1b099d2dd746c4c0e4
sorasora46 <sorrawit02546@gmail.com>
2024-07-28 15:21:37 +0700

    Implemented CustomException when data not found

--- src/main/java/me/sora/eCommerce/service/ProductService.java
+++ src/main/java/me/sora/eCommerce/service/ProductService.java
@@ -1,9 +1,12 @@
 package me.sora.eCommerce.service;
 
 import lombok.RequiredArgsConstructor;
+import me.sora.eCommerce.constant.ErrorConstant;
+import me.sora.eCommerce.controller.advice.CustomException;
 import me.sora.eCommerce.dto.Product.GetProductResponse;
 import me.sora.eCommerce.mapper.ProductMapper;
 import me.sora.eCommerce.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +16,8 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public GetProductResponse getProductById(String id) {
-        var product = productRepository.findById(id).orElse(null);
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorConstant.DATA_NOT_FOUND, HttpStatus.NOT_FOUND));
         return ProductMapper.INSTANCE.fromProductEntityToGetProductResponse(product);
     }
 
