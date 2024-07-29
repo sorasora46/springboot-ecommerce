7887a9fe871304d83a7cae79e6dc0618495e4b4d
sorasora46 <sorrawit02546@gmail.com>
2024-07-29 23:08:05 +0700

    Added mappers for create product endpoint

--- src/main/java/me/sora/eCommerce/mapper/ProductMapper.java
+++ src/main/java/me/sora/eCommerce/mapper/ProductMapper.java
@@ -1,8 +1,11 @@
 package me.sora.eCommerce.mapper;
 
+import me.sora.eCommerce.dto.Product.CreateProductRequest;
+import me.sora.eCommerce.dto.Product.CreateProductResponse;
 import me.sora.eCommerce.dto.Product.GetProductResponse;
 import me.sora.eCommerce.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,5 +13,11 @@ public interface ProductMapper {
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
     GetProductResponse fromProductEntityToGetProductResponse(Product product);
+
+    @Mapping(target = "createdBy", source = "createdBy")
+    @Mapping(target = "updatedBy", source = "createdBy")
+    Product fromCreateProductRequestToProductEntity(CreateProductRequest request, String createdBy);
+
+    CreateProductResponse fromProductEntityToCreateProductResponse(Product product);
 
 }
