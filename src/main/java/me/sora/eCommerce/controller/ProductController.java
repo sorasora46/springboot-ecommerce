f234071b56918ca828c7914a637598f868eb93f4
sorasora46 <sorrawit02546@gmail.com>
2024-07-29 22:17:07 +0700

    Initiated create product endpoint

--- src/main/java/me/sora/eCommerce/controller/ProductController.java
+++ src/main/java/me/sora/eCommerce/controller/ProductController.java
@@ -1,15 +1,14 @@
 package me.sora.eCommerce.controller;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import me.sora.eCommerce.dto.CommonResponse;
+import me.sora.eCommerce.dto.Product.CreateProductRequest;
 import me.sora.eCommerce.dto.Product.GetProductResponse;
 import me.sora.eCommerce.service.ProductService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static me.sora.eCommerce.constant.ApiConstant.ApiStatus.SUCCESS;
 
@@ -24,6 +23,10 @@ public class ProductController {
     public ResponseEntity<CommonResponse<GetProductResponse>> getProductById(@NotEmpty @PathVariable String id) {
         var response = productService.getProductById(id);
         return ResponseEntity.ok().body(CommonResponse.of(SUCCESS, response));
+    }
+
+    @PostMapping()
+    public ResponseEntity<CommonResponse<Object>> createProduct(@Valid @RequestBody CreateProductRequest request) {
     }
 
 }
