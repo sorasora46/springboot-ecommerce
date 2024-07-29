80b51869af98c10f523a8efbee27412c8d0ea4d7
sorasora46 <sorrawit02546@gmail.com>
2024-07-30 01:31:37 +0700

    Implemented create user endpoint

--- src/main/java/me/sora/eCommerce/controller/ProductController.java
+++ src/main/java/me/sora/eCommerce/controller/ProductController.java
@@ -3,12 +3,21 @@ package me.sora.eCommerce.controller;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
+import me.sora.eCommerce.constant.ErrorConstant;
+import me.sora.eCommerce.controller.advice.CustomException;
 import me.sora.eCommerce.dto.CommonResponse;
 import me.sora.eCommerce.dto.Product.CreateProductRequest;
+import me.sora.eCommerce.dto.Product.CreateProductResponse;
 import me.sora.eCommerce.dto.Product.GetProductResponse;
 import me.sora.eCommerce.service.ProductService;
+import me.sora.eCommerce.util.AuthUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 import static me.sora.eCommerce.constant.ApiConstant.ApiStatus.SUCCESS;
 
@@ -26,7 +35,19 @@ public class ProductController {
     }
 
     @PostMapping()
-    public ResponseEntity<CommonResponse<Object>> createProduct(@Valid @RequestBody CreateProductRequest request) {
+    public ResponseEntity<CommonResponse<CreateProductResponse>> createProduct(
+            @Valid @RequestBody CreateProductRequest request,
+            @AuthenticationPrincipal UserDetails userDetails) {
+
+        if (!AuthUtils.hasAdminPermission(userDetails)) {
+            throw new CustomException(ErrorConstant.PERMISSION_NOT_ALLOW, HttpStatus.UNAUTHORIZED);
+        }
+
+        var response = productService.createProduct(request, userDetails.getUsername());
+        var productId = response.getId();
+        return ResponseEntity
+                .created(URI.create(productId))
+                .body(CommonResponse.of(SUCCESS, response));
     }
 
 }
--- src/main/java/me/sora/eCommerce/service/ProductService.java
+++ src/main/java/me/sora/eCommerce/service/ProductService.java
@@ -3,6 +3,8 @@ package me.sora.eCommerce.service;
 import lombok.RequiredArgsConstructor;
 import me.sora.eCommerce.constant.ErrorConstant;
 import me.sora.eCommerce.controller.advice.CustomException;
+import me.sora.eCommerce.dto.Product.CreateProductRequest;
+import me.sora.eCommerce.dto.Product.CreateProductResponse;
 import me.sora.eCommerce.dto.Product.GetProductResponse;
 import me.sora.eCommerce.mapper.ProductMapper;
 import me.sora.eCommerce.repository.ProductRepository;
@@ -19,6 +21,12 @@ public class ProductService {
         var product = productRepository.findById(id)
                 .orElseThrow(() -> new CustomException(ErrorConstant.DATA_NOT_FOUND, HttpStatus.NOT_FOUND));
         return ProductMapper.INSTANCE.fromProductEntityToGetProductResponse(product);
+    }
+
+    public CreateProductResponse createProduct(CreateProductRequest request, String username) {
+        var newProduct = ProductMapper.INSTANCE.fromCreateProductRequestToProductEntity(request, username);
+        var savedProduct = productRepository.save(newProduct);
+        return ProductMapper.INSTANCE.fromProductEntityToCreateProductResponse(savedProduct);
     }
 
 }
