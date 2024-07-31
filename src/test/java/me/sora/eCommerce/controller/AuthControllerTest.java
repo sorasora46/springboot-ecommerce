7e33cb2910a70817e47220be15d38d7e538cf992
sorasora46 <sorrawit02546@gmail.com>
2024-07-31 18:00:32 +0700

    Create AuthControllerTest.java

---
+++ src/test/java/me/sora/eCommerce/controller/AuthControllerTest.java
@@ -0,0 +1,147 @@
+package me.sora.eCommerce.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.sora.eCommerce.config.AuthenticationFilter;
+import me.sora.eCommerce.constant.AuthConstant;
+import me.sora.eCommerce.dto.Authentication.AuthenticationRequest;
+import me.sora.eCommerce.dto.Authentication.AuthenticationResponse;
+import me.sora.eCommerce.dto.Authentication.RegisterRequest;
+import me.sora.eCommerce.dto.Authentication.RegisterResponse;
+import me.sora.eCommerce.service.AuthenticationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+import static me.sora.eCommerce.constant.ApiConstant.ApiStatus.FAILED;
+import static me.sora.eCommerce.constant.ApiConstant.ApiStatus.SUCCESS;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(controllers = AuthController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {AuthenticationFilter.class}))
+@ExtendWith(MockitoExtension.class)
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void givenRequest_whenRegister_thenSuccess() throws Exception {
+        // Given
+        var now = Instant.now();
+        var request = RegisterRequest.builder()
+                .username("username")
+                .password("password")
+                .role(AuthConstant.Role.USER.name())
+                .birthDate(LocalDate.now())
+                .email("email")
+                .firstName("firstName")
+                .lastName("lastName")
+                .phoneNo("phoneNo")
+                .build();
+        var response = RegisterResponse.builder()
+                .id("id")
+                .createdDate(now)
+                .token("token")
+                .build();
+
+        // When
+        when(authenticationService.register(any(RegisterRequest.class))).thenReturn(response);
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/auth/register")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(SUCCESS))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.id").value(response.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.createdDate").value(response.getCreatedDate().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.token").value(response.getToken()));
+    }
+
+    @Test
+    void givenInvalidRequest_whenRegister_thenBadRequestError() throws Exception {
+        // Given
+        var request = RegisterRequest.builder().build();
+
+        // When
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/auth/register")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(FAILED))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.errors").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.errors").isNotEmpty());
+    }
+
+    @Test
+    void givenRequest_whenLogin_thenSuccess() throws Exception {
+        // Given
+        var request = AuthenticationRequest.builder()
+                .username("username")
+                .password("password")
+                .build();
+        var response = AuthenticationResponse.builder()
+                .token("token")
+                .build();
+
+        // When
+        when(authenticationService.authenticate(any(AuthenticationRequest.class))).thenReturn(response);
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/auth/login")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(SUCCESS))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.token").value(response.getToken()));
+    }
+
+    @Test
+    void givenInvalidRequest_whenLogin_thenBadRequestError() throws Exception {
+        // Given
+        var request = AuthenticationRequest.builder().build();
+
+        // When
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/auth/login")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(FAILED))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.errors").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.errors").isNotEmpty());
+    }
+
+}
