acc163d6eb04b6d4a60ebff1ef7362e5980d2570
sorasora46 <sorrawit02546@gmail.com>
2024-07-31 16:00:20 +0700

    Added user entity to mapper

--- src/main/java/me/sora/eCommerce/mapper/ProductMapper.java
+++ src/main/java/me/sora/eCommerce/mapper/ProductMapper.java
@@ -4,6 +4,7 @@ import me.sora.eCommerce.dto.Product.CreateProductRequest;
 import me.sora.eCommerce.dto.Product.CreateProductResponse;
 import me.sora.eCommerce.dto.Product.GetProductResponse;
 import me.sora.eCommerce.entity.Product;
+import me.sora.eCommerce.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -16,7 +17,7 @@ public interface ProductMapper {
 
     @Mapping(target = "createdBy", source = "createdBy")
     @Mapping(target = "updatedBy", source = "createdBy")
-    Product fromCreateProductRequestToProductEntity(CreateProductRequest request, String createdBy);
+    Product fromCreateProductRequestToProductEntity(CreateProductRequest request, User createdBy);
 
     CreateProductResponse fromProductEntityToCreateProductResponse(Product product);
 
--- src/main/java/me/sora/eCommerce/service/ProductService.java
+++ src/main/java/me/sora/eCommerce/service/ProductService.java
@@ -8,6 +8,7 @@ import me.sora.eCommerce.dto.Product.CreateProductResponse;
 import me.sora.eCommerce.dto.Product.GetProductResponse;
 import me.sora.eCommerce.mapper.ProductMapper;
 import me.sora.eCommerce.repository.ProductRepository;
+import me.sora.eCommerce.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     public GetProductResponse getProductById(String id) {
         var product = productRepository.findById(id)
@@ -24,7 +26,9 @@ public class ProductService {
     }
 
     public CreateProductResponse createProduct(CreateProductRequest request, String username) {
-        var newProduct = ProductMapper.INSTANCE.fromCreateProductRequestToProductEntity(request, username);
+        var createdBy = userRepository.findByUsername(username)
+                .orElseThrow(() -> new CustomException(ErrorConstant.DATA_NOT_FOUND, HttpStatus.NOT_FOUND));
+        var newProduct = ProductMapper.INSTANCE.fromCreateProductRequestToProductEntity(request, createdBy);
         var savedProduct = productRepository.save(newProduct);
         return ProductMapper.INSTANCE.fromProductEntityToCreateProductResponse(savedProduct);
     }
