882676fb46993f3b5ca6c8c3aab7e526155ef164
sorasora46 <sorrawit02546@gmail.com>
2024-07-31 17:40:40 +0700

    Create ValidationErrorResponse.java

---
+++ src/main/java/me/sora/eCommerce/dto/Validation/ValidationErrorResponse.java
@@ -0,0 +1,20 @@
+package me.sora.eCommerce.dto.Validation;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class ValidationErrorResponse {
+    private List<ValidationError> errors;
+
+    @Data
+    @Builder
+    public static class ValidationError {
+        private String field;
+        private String message;
+        private String providedValue;
+    }
+}
