82e9878cc27ac4df12430f78ac68d7922c5cdc4c
sorasora46 <sorrawit02546@gmail.com>
2024-08-03 12:39:22 +0700

    Init CartService

---
+++ src/main/java/me/sora/eCommerce/service/CartService.java
@@ -0,0 +1,22 @@
+package me.sora.eCommerce.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+
+    public Object getCart(String username) {
+        return null;
+    }
+
+    public Object addToCart() {
+        return null;
+    }
+
+    public Object removeFromCart() {
+        return null;
+    }
+
+}
