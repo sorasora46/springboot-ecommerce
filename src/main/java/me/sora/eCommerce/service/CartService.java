4e967ddc49db628a5dcfcc711db213eb7d453a94
sorasora46 <sorrawit02546@gmail.com>
2024-08-03 12:53:36 +0700

    Implemented getCart service

--- src/main/java/me/sora/eCommerce/service/CartService.java
+++ src/main/java/me/sora/eCommerce/service/CartService.java
@@ -1,14 +1,27 @@
 package me.sora.eCommerce.service;
 
 import lombok.RequiredArgsConstructor;
+import me.sora.eCommerce.constant.ErrorConstant;
+import me.sora.eCommerce.controller.advice.CustomException;
+import me.sora.eCommerce.dto.Product.GetProductResponse;
+import me.sora.eCommerce.repository.CartRepository;
+import me.sora.eCommerce.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
 
-    public Object getCart(String username) {
-        return null;
+    private final CartRepository cartRepository;
+    private final UserRepository userRepository;
+
+    public List<GetProductResponse> getCart(String username) {
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new CustomException(ErrorConstant.DATA_NOT_FOUND, HttpStatus.NOT_FOUND));
+        return cartRepository.findByUser(user).orElse(null);
     }
 
     public Object addToCart() {
