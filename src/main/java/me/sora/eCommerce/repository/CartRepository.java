3d3cea2934a462dccc23652fb7a46e6d5373f7a8
sorasora46 <sorrawit02546@gmail.com>
2024-08-03 12:49:43 +0700

    Added findByUser query to get all products in cart

--- src/main/java/me/sora/eCommerce/repository/CartRepository.java
+++ src/main/java/me/sora/eCommerce/repository/CartRepository.java
@@ -1,9 +1,27 @@
 package me.sora.eCommerce.repository;
 
+import me.sora.eCommerce.dto.Product.GetProductResponse;
 import me.sora.eCommerce.entity.Cart;
+import me.sora.eCommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, String> {
+
+    @Query("SELECT new me.sora.eCommerce.dto.Product.GetProductResponse(" +
+            "p.id," +
+            "p.name, " +
+            "p.description, " +
+            "p.price, " +
+            "p.createdDate, " +
+            "p.updatedDate) " +
+            "FROM carts c, cart_items ci, products p " +
+            "WHERE c.user = :user AND ci.cart = c AND ci.product = p")
+    List<GetProductResponse> findByUser(@Param("user") User user);
+
 }
