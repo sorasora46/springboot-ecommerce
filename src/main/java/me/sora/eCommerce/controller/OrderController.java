0f9cc5c78523360f5c9228c42d512222e3f111bd
sorasora46 <sorrawit02546@gmail.com>
2024-08-04 15:35:58 +0700

    Renamed controller

--- src/main/java/me/sora/eCommerce/controller/OrderController.java
+++ src/main/java/me/sora/eCommerce/controller/OrderController.java
@@ -9,20 +9,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/checkout")
 @RequiredArgsConstructor
-public class CheckoutController {
+public class OrderController {
 
-    @GetMapping("/{checkoutId}")
-    public Object getCheckoutById(@NotEmpty @PathVariable String checkoutId) {
+    @GetMapping("/{orderId}")
+    public Object getOrderById(@NotEmpty @PathVariable String orderId) {
         return null;
     }
 
     @GetMapping()
-    public Object getCheckouts(@AuthenticationPrincipal UserDetails userDetails) {
+    public Object getOrders(@AuthenticationPrincipal UserDetails userDetails) {
         return null;
     }
 
     @PostMapping()
-    public Object createCheckout(@AuthenticationPrincipal UserDetails userDetails) {
+    public Object createOrder(@AuthenticationPrincipal UserDetails userDetails) {
         return null;
     }
 
