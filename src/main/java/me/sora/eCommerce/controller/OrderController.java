5a99817343a090d03f311c425f78b6801fa9ab00
sorasora46 <sorrawit02546@gmail.com>
2024-08-15 23:56:11 +0700

    Added get order by id to controller

--- src/main/java/me/sora/eCommerce/controller/OrderController.java
+++ src/main/java/me/sora/eCommerce/controller/OrderController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import me.sora.eCommerce.dto.CommonResponse;
 import me.sora.eCommerce.dto.Order.CreateOrderRequest;
 import me.sora.eCommerce.dto.Order.CreateOrderResponse;
+import me.sora.eCommerce.dto.Order.GetOrderByIdResponse;
 import me.sora.eCommerce.service.OrderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,8 +25,9 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping("/{orderId}")
-    public Object getOrderById(@NotEmpty @PathVariable String orderId) {
-        return null;
+    public ResponseEntity<CommonResponse<GetOrderByIdResponse>> getOrderById(@NotEmpty @PathVariable String orderId) {
+        var response = orderService.getOrderById(orderId);
+        return ResponseEntity.ok().body(CommonResponse.of(SUCCESS, response));
     }
 
     @GetMapping()
